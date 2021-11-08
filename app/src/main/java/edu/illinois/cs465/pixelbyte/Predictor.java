698d57da7232ca544005862e25aa0514df23588c
Sonali Merchia <sonalimerchia@yahoo.com>
2021-11-08 15:49:41 -0600

    fix copy-paste bugs

--- app/src/main/java/edu/illinois/cs465/pixelbyte/Predictor.java
+++ app/src/main/java/edu/illinois/cs465/pixelbyte/Predictor.java
@@ -27,7 +27,7 @@ public class Predictor extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.predictor);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
 //        setSupportActionBar(toolbar);S
 
