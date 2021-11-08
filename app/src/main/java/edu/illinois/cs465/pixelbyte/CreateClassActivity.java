7659b0e2b72b95d7923a1215f073379ecc09b274
arafol2 <arafol2@illinois.edu>
2021-11-08 14:12:47 -0600

    createed activity layout pages

---
+++ app/src/main/java/edu/illinois/cs465/pixelbyte/CreateClassActivity.java
@@ -0,0 +1,107 @@
+package edu.illinois.cs465.pixelbyte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class CreateClassActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+    public static final String CREATE_CLASS_RETURN_MSG = "CREATE_CLASS_RETURN_MSG";
+
+    private TextView addClassTitle;
+    private TextView courseNameLabel;
+    private TextView courseColorLabel;
+    private TextView courseDeptLabel;
+
+    private EditText courseNameInput;
+
+    private Button redButton;
+    private Button greenButton;
+    private Button blueButton;
+
+    private Button nextButton;
+
+    private Spinner courseDeptSpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_class);
+
+        //TextViews
+        addClassTitle = (TextView) findViewById(R.id.add_course_title);
+        courseNameLabel = (TextView) findViewById(R.id.course_name_label);
+        courseColorLabel = (TextView) findViewById(R.id.course_color_label);
+        courseDeptLabel = (TextView) findViewById(R.id.course_department_label);
+
+        addClassTitle.setText(R.string.activity_title_add_course);
+        courseNameLabel.setText(R.string.course_name_label);
+        courseColorLabel.setText(R.string.course_color_label);
+        courseDeptLabel.setText(R.string.course_department_label);
+
+        //EditText
+        courseNameInput = (EditText) findViewById(R.id.course_edit_name);
+        courseNameInput.setAllCaps(false);
+        courseNameInput.setHint(R.string.course_name_hint);
+
+        //Color Buttons
+        redButton = (Button) findViewById(R.id.red_button);
+        greenButton = (Button) findViewById(R.id.green_button);
+        blueButton = (Button) findViewById(R.id.blue_button);
+
+        redButton.setOnClickListener(this);
+        greenButton.setOnClickListener(this);
+        blueButton.setOnClickListener(this);
+
+        //Navigation Buttons
+        nextButton = (Button) findViewById(R.id.next_button);
+
+        nextButton.setOnClickListener(this);
+
+        //Spinner (Drop-down menu)
+        courseDeptSpinner = (Spinner) findViewById(R.id.course_dept_spinner);
+        courseDeptSpinner.setOnItemSelectedListener(this);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.class_departments_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        courseDeptSpinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.red_button) {
+            // Change Class theme to red
+        }
+        if (v.getId() == R.id.green_button) {
+            // Change Class theme to green
+        }
+        if (v.getId() == R.id.blue_button) {
+            // Change Class theme to bllue
+        }
+
+        if (v.getId() == R.id.next_button) {
+            // Go to next fragment/activity
+        }
+    }
+}
--- app/src/main/java/edu/illinois/cs465/pixelbyte/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/pixelbyte/MainActivity.java
@@ -1,14 +1,39 @@
 package edu.illinois.cs465.pixelbyte;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {//implements View.OnClickListener {
+    private static final int CREATE_CLASS_CODE = 1;
+
+    private Button addClassButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buttons on Home Screen
+        addClassButton = (Button) findViewById(R.id.add_class_button);
+
+        //OnClick Listeners
+        //addClassButton.setOnClickListener(this);
     }
+
+    /*
+    public void onClick (View v) {
+        if (v.getId() == R.id.add_class_button) {
+
+            Intent intent = new Intent (MainActivity.this, CreateClassActivity.class);
+            startActivity(intent);
+        }
+
+    }
+     */
 }
+
