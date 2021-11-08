3246b15126b1be60a504bae27697635bc07ae28e
arafol2 <arafol2@illinois.edu>
2021-11-08 15:12:51 -0600

    Cleared MainActivity for easier merge

--- app/src/main/java/edu/illinois/cs465/pixelbyte/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/pixelbyte/MainActivity.java
@@ -8,32 +8,13 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {//implements View.OnClickListener {
-    private static final int CREATE_CLASS_CODE = 1;
-
-    private Button addClassButton;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Buttons on Home Screen
-        addClassButton = (Button) findViewById(R.id.add_class_button);
-
-        //OnClick Listeners
-        //addClassButton.setOnClickListener(this);
     }
-
-    /*
-    public void onClick (View v) {
-        if (v.getId() == R.id.add_class_button) {
-
-            Intent intent = new Intent (MainActivity.this, CreateClassActivity.class);
-            startActivity(intent);
-        }
-
-    }
-     */
 }
 
