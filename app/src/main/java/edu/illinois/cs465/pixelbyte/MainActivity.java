eedabdc27db4fb7f337af5973d43fe359230e116
Sonali Merchia <42362942+sonalimerchia@users.noreply.github.com>
2021-11-12 11:16:26 -0600

    Added functionality (#6)
    Co-authored-by: Will Andersen <wra3@illinois.edu>

--- app/src/main/java/edu/illinois/cs465/pixelbyte/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/pixelbyte/MainActivity.java
@@ -2,6 +2,8 @@ package edu.illinois.cs465.pixelbyte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
@@ -80,5 +83,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Open add category sheet
             openDialog(BottomSheetCodes.AddCategory, "Add Category");
         }
+        Button classButton = (Button) (findViewById(R.id.MainButton));
+        try {
+            FileOutputStream outputStream = openFileOutput("Class.cf", Context.MODE_PRIVATE);
+            String str = "CS 465\n" +
+                    "90.0%\n" +
+                    "Help can be found on canvas.illinois.edu\n" +
+                    "3\n" +
+                    "\n" +
+                    "Quizzes\n" +
+                    "30%\n" +
+                    "Scheme...\n" +
+                    "3\n" +
+                    "Quiz_1\t90\n" +
+                    "Quiz_2\t98\n" +
+                    "Quiz_3\t92\n" +
+                    "\n" +
+                    "Tests\n" +
+                    "40%\n" +
+                    "Scheme...\n" +
+                    "2\n" +
+                    "Test_1\t90\n" +
+                    "Test_2\t98\n" +
+                    "\n" +
+                    "Projects\n" +
+                    "30%\n" +
+                    "Scheme...\n" +
+                    "1\n" +
+                    "Main_Project\t85";
+            outputStream.write(str.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {}
+
+        Intent intent = new Intent(this, ClassActivity.class);
+        intent.putExtra("ClassName", "CS 465");
+        classButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
